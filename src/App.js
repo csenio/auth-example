a2e1e55b2d4769f9a6e848452a67a793c2483adf
jescowuester <jesco@storyofams.com>
2019-06-21 18:50:19 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Protected() {
+  return <h2>Protected</h2>;
+}
+
+function NotAllowed() {
+  return <h2>NOT ALLOWED</h2>;
+}
+
+const AuthenticatedRouter = ({ children }) => (
+  <Router>
+    <Route path="/" exact component={Index} />
+    <Route path="/about/" component={About} />
+    <Route path="/protected/" component={Protected} />
+    {children}
+  </Router>
+);
+
+const UnAuthenticatedRouter = ({ children }) => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Index} />
+      <Route path="/about/" component={About} />
+      <Route component={NotAllowed} />
+    </Switch>
+
+    {children}
+  </Router>
+);
 
 function App() {
+  const [isAuth, setAuth] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isAuth ? (
+        <AuthenticatedRouter>
+          {" "}
+          <Nav />
+        </AuthenticatedRouter>
+      ) : (
+        <UnAuthenticatedRouter>
+          <Nav />
+        </UnAuthenticatedRouter>
+      )}
+      <button onClick={() => setAuth(a => !a)}>
+        {isAuth ? "log out" : "log in"}
+      </button>
     </div>
   );
 }
+
+const Nav = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/about/">About</Link>
+    </li>
+    <li>
+      <Link to="/protected/">protected</Link>
+    </li>
+  </ul>
+);
 
 export default App;
